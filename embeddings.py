2ee26d5f97ea668628b2cbf691d51177a1f07638
RevazRevazashvili <revazi.revazashvili380@ens.tsu.edu.ge>
2024-05-09 15:50:28 +0400

    updated embeddings

--- embeddings.py
+++ embeddings.py
@@ -8,27 +8,7 @@ import document_processing
 
 os.environ['PINECONE_API_KEY'] = 'your pinecone api here'
 
-
-# function to load pdf document
-def load_docs(directory):
-    loader = PyPDFLoader(directory)
-    documents = loader.load()
-    return documents
-
-
-# load document
-# not use this if you are using document processing else uncomment
-# documents = load_docs('your pdf file here')
-
-
-# splitting document with chunk_size
-def split_docs(documents, chunk_size=500, chunk_overlap=20):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-    docs = text_splitter.split_documents(documents)
-    return docs
-
-
-# docs = split_docs(documents)  # text spliter
+#load data
 docs = document_processing.docs_list
 
 
